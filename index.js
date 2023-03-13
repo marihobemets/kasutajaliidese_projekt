9efe0df55484970a3122e21e6007c53a658a07f5
Mari Hõbemets <90192502+marihobemets@users.noreply.github.com>
2023-03-13 21:15:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ window.onscroll = () => {
     const nav = document.querySelector('#navbar');
     if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
 };
+
+const btn=document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+   alert("You clicked me");
+})
