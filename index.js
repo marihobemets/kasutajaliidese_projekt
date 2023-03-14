d29e2de2113977351f5b8bf477404c269314038b
Mari Hõbemets <90192502+marihobemets@users.noreply.github.com>
2023-03-14 18:08:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-window.onscroll = () => {
-    const nav = document.querySelector('#navbar');
-    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const sectionOneOptions = {
+    rootMargin: "-20px 0px 0px 0px"
 };
+
+const sectionOneObserver = new IntersectionObserver(function(
+    entries, 
+    sectionOneObserver
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            header.classList.add("nav-scrolled");
+        } else {
+            header.classList.remove("nav-scrolled");
+        }
+    })
+}, sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne); 
+
+
+
+
+
+
+
+$('#myDropdown').on('show.bs.dropdown', function () {
+    // do something...
+})
 
 const btn=document.getElementById("btn");
 
